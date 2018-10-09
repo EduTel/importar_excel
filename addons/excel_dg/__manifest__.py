b58a7af1f4d74b6f771c11091938a8b075f32c94
EduTel <15623704+EduTel@users.noreply.github.com>
2018-10-09 18:55:02 -0500

    excel

---
+++ addons/excel_dg/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Excel Modulo Desiteg",
+    "depends": [
+        "base"
+    ],
+    "application": True,
+    "version": "3.0",
+    'author': "Eduardo tellez 1",
+    'description': """cargar libreria python y subir archivos csv""",
+     "data": [
+        "views/views.xml"
+    ],
+    "css":[
+        "css.css"
+    ],
+}
