60794b5f333e161f50ef1e8b8e780b35ea161cb9
root <root@eduardo-desktop>
2018-11-14 19:01:42 -0600

    cambios

---
+++ addons/asset_dg/__manifest__.py
@@ -0,0 +1,30 @@
+{
+    "name": "Assets Desiteg",
+    "maintainer": "Desiteg",
+    "depends": [
+        "base",
+        "account_asset"
+        #"account"
+        #"report",
+        #"purchase",
+        #'barcodes',
+        #'mail',
+        # 'sale_stock'
+    ],
+    'summary': 'Assets Desiteg',
+    "application": True,
+    'description': """
+        Assets Desiteg
+        ==============================================
+    """,
+    'website': '',
+    "images": [],
+    "data": [
+        "views/views.xml"
+    ],
+    "css": [
+        "css/index.css"
+    ],
+    "version": "0.1",
+    'auto_install': True,
+}
---
+++ addons/asset_dg/models/models.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import sys
+import coloredlogs
+import logging
+import copy
+from datetime import datetime
+import odoo
+from odoo import exceptions
+from odoo import models, fields, api, _
+from mako.template import Template
+from odoo.exceptions import Warning, UserError, ValidationError
+logger = logging.getLogger(__name__)
+
+class Assetadd(models.Model):
+    """Nuevo modulo de Odoo Team"""
+    coloredlogs.install(level='DEBUG')
+    _description = 'Agregar campo'
+    _inherit = ['account.asset.asset']
+    logger.warning("====================================================Iniciando %s")
+    notes = fields.Text(string="Observaciones", size=250, readonly=False, required=False, index=False, copy=True, store=True)
